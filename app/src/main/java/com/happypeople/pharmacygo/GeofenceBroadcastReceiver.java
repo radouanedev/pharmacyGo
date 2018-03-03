f5d6933ea801caa6c5b4cca1e15ca8e994fa94d6
radouane dev <radouanedev@gmail.com>
2018-03-03 12:27:38 +0000

    last modif

--- app/src/main/java/com/happypeople/pharmacygo/GeofenceBroadcastReceiver.java
+++ app/src/main/java/com/happypeople/pharmacygo/GeofenceBroadcastReceiver.java
@@ -3,18 +3,32 @@ package com.happypeople.pharmacygo;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
 
 /**
  * Created by Radouane on 30/01/2018.
  */
-
 public class GeofenceBroadcastReceiver extends BroadcastReceiver {
 
+    public static final String TAG = GeofenceBroadcastReceiver.class.getSimpleName();
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
 
+        if(geofencingEvent.hasError()) {
+            Log.e(TAG, String.format("Error code : %d", geofencingEvent.getErrorCode()));
+            return;
+        }
+
+        int geofenceTransition = geofencingEvent.getGeofenceTransition();
+
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER);
+        else if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT);
 
     }
 }
